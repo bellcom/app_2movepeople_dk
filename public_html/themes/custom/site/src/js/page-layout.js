5fb531cbd94866e4c798fbff5abb864de9d8ae22
Evgeny Varlamov <ev@bellcom.ee>
2017-08-21 09:53:18 +0300

    added theme

---
+++ public_html/themes/custom/site/src/js/page-layout.js
@@ -0,0 +1,133 @@
+// |--------------------------------------------------------------------------
+// | Page layout
+// |--------------------------------------------------------------------------
+// |
+// | This jQuery script is written by
+// | Morten Nissen
+// |
+
+var pageLayout = (function ($) {
+    'use strict';
+
+    var pub = {};
+
+    /**
+     * Instantiate
+     */
+    pub.init = function () {
+        registerBootEventHandlers();
+        registerEventHandlers();
+    };
+
+    /**
+     * Register boot event handlers
+     */
+    function registerBootEventHandlers() {}
+
+    /**
+     * Register event handlers
+     */
+    function registerEventHandlers() {
+
+        if ( ! Modernizr.touchevents) {
+
+            $(window).on('load', function () {
+                footerAttached();
+            });
+
+            $(window).on('resize', function () {
+                footerAttached();
+            });
+        }
+    }
+
+    /**
+     * Footer attached
+     */
+    function footerAttached() {
+        if ($('body').hasClass('footer-attached')) {
+            var $footer = $('.footer');
+            var footerHeight = $footer.outerHeight(true);
+
+            $('.inner-wrapper').css('padding-bottom', footerHeight);
+        }
+    }
+
+    return pub;
+})(jQuery);
+
+
+/* JS for house illustration on services page */
+
+// Top
+jQuery(document).ready(function($){
+$("#top").mouseenter(function () { // show  
+    $(".info-modal").show();
+});
+});
+
+jQuery(document).ready(function($){
+$(".info-modal").mouseleave(function () { // hide  on mouse out
+    $(".info-modal").hide();
+});
+    });
+jQuery(document).ready(function($){    
+$(".middle, .bottom").mouseenter(function () { // show  
+    $(".info-modal").hide();
+});
+    });
+// Left
+jQuery(document).ready(function($){    
+$("#left").mouseenter(function () { // show  
+    $(".info-modal-left").show();
+});
+    });
+jQuery(document).ready(function($){    
+$(".info-modal-left").mouseleave(function () { // hide on mouse out
+    $(".info-modal-left").hide();
+});
+    });
+jQuery(document).ready(function($){    
+$(".right, .bottom, .top").mouseenter(function () { // show  
+    $(".info-modal-left").hide();
+});
+    });
+// Right
+jQuery(document).ready(function($){   
+$("#right").mouseenter(function () { // show  
+    $(".info-modal-right").show();
+});
+    });
+jQuery(document).ready(function($){   
+$(".info-modal-right").mouseleave(function () { // hide on mouse out 
+    $(".info-modal-right").hide();
+});
+    });
+jQuery(document).ready(function($){   
+$(".left, .bottom, .top").mouseenter(function () { // show  
+    $(".info-modal-right").hide();
+});
+    });
+// Bottom
+jQuery(document).ready(function($){   
+$("#bottom").mouseenter(function () { // show  
+    $(".info-modal-bottom").show();
+});
+    });
+jQuery(document).ready(function($){ 
+$(".info-modal-bottom").mouseleave(function () { // hide on mouse out
+    $(".info-modal-bottom").hide();
+});
+    });
+jQuery(document).ready(function($){ 
+$(".right, .left, .top").mouseenter(function () { // show  
+    $(".info-modal-bottom").hide();
+});
+    });
+
+// Overall action / Just for UX purpose
+jQuery(document).ready(function($){ 
+$("body").click(function () { // show  
+    $(".info-modal-bottom, .info-modal-left, .info-modal-right, .info-modal").hide();
+});
+    });
