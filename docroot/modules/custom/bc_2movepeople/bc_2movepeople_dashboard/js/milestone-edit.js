83fca3adb883c93160f1d4f5c746fc4891e8e1c3
Stanislav Kutasevits <stan@bellcom.dk>
2017-08-25 15:19:15 +0300

    updated dashboard layout, milestones now has bootstrap accordion

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/js/milestone-edit.js
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/js/milestone-edit.js
@@ -1,18 +1,16 @@
 (function ($, Drupal) {
     'use strict';
-    
+
     var url = document.location.toString();
     var anchor = '';
     if (url.indexOf("#") > 0) {
-      anchor = url.substring(url.indexOf("#")+1);
+        anchor = url.substring(url.indexOf("#") + 1);
     }
-    
-    $(window).load(function() {
-      if (anchor) {
-        var elem = $("h3[data-progression-id="+anchor+"]");
-        var index = $("h3.ui-accordion-header").index(elem);
-        $( "#accordion" ).accordion( "option", "active", index );
-      }
-    });
 
+    if (anchor) {
+        $('.collapse').collapse('hide');
+        $('#accordion-milestones-collapse-' + anchor).collapse('show');
+    } else {
+        $('.collapse').first().collapse('show');
+    }
 })(jQuery, Drupal);
