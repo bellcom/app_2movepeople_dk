6d16d0200f66de50187dc09418d22b46d8562b32
Stanislav Kutasevits <stan@bellcom.dk>
2017-09-25 16:06:28 +0300

    2M-112 fixing charset for outgoing emails

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
@@ -644,7 +644,7 @@ class MovepeopleDashboardController extends ControllerBase {
   public static function sendMail($message) {
     $send_mail = new \Drupal\Core\Mail\Plugin\Mail\PhpMail(); 
     $message['headers'] = array(
-      'content-type' => 'text/html',
+      'content-type' => 'text/html; charset=UTF-8; format=flowed; delsp=yes',
       'MIME-Version' => '1.0',
       'reply-to' => $message['from'],
       'from' => $message['sender'].' <'.$message['from'].'>'
