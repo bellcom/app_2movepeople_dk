572edf86ffb45db8e170619d5e70f6c39d5558d2
Dmitri Serõn <ds@bellcom.ee>
2018-02-06 11:22:48 +0200

    M2-187 Settings to separate feedback and rates

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople/src/Form/ConfigForm.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople/src/Form/ConfigForm.php
@@ -56,6 +56,12 @@ class ConfigForm extends ConfigFormBase {
       '#title' => $this->t('User email is required'),
       '#default_value' => $config->get('email_required'),
     ];
+    
+    $form['functionality']['rates_separately'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show feedback rates and progressions rates separately'),
+      '#default_value' => $config->get('rates_separately'),
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -67,6 +73,7 @@ class ConfigForm extends ConfigFormBase {
     $this->config('bc_2movepeople.settings')
       ->set('enable_milestones', $form_state->getValue('enable_milestones'))
       ->set('email_required', $form_state->getValue('email_required'))
+      ->set('rates_separately', $form_state->getValue('rates_separately'))
       ->save();
 
     parent::submitForm($form, $form_state);
