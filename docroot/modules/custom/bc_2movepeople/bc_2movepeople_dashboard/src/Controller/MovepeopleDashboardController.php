8047849e711d5fd170c6cd8bff0c2f1ac4b9d409
Stanislav Kutasevits <stan@bellcom.dk>
2018-03-12 13:23:02 +0200

    2M-208 Progression caterogies that are shown in the FIT tab are not to be shown on the progression overview

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
@@ -255,7 +255,7 @@ class MovepeopleDashboardController extends ControllerBase {
       "#user" => $user->id(),
     );
 
-    $entity_progression_ids = array_keys($this->getProgressionTargets($user->id(), 'progressions'));
+    $entity_progression_ids = array_keys($this->getProgressionTargets($user->id(), 'progression'));
     $entity_milestone_ids = array_keys($this->getProgressionTargets($user->id(), 'target_milestone'));
 
     if (!empty($entity_progression_ids)) {
