2095984f26497ea384e79cafa3ce5f267dc4d196
Juulia Devadze <jd@2move>
2018-06-27 08:29:00 +0200

    Code restructure

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
@@ -511,15 +511,14 @@ class MovepeopleDashboardController extends ControllerBase {
 
     foreach ($entity_ids as $key => $target_id) {
       $nodedata = \Drupal::entityTypeManager()->getStorage('node')->load($target_id);
-      if (!is_object($nodedata)) continue;
-      $title = $nodedata->get('title')->value;
-      if ($nodedata->getType() === 'progression_target') {
-         $priority_edit_form = new MilestonePriorityEditForm($nodedata);
-         $priority = \Drupal::formBuilder()->getForm($priority_edit_form);
+      if (is_object($nodedata) && $nodedata->getType() === 'progression_target') {
+        $title = $nodedata->get('title')->value;
+        $priority_edit_form = new MilestonePriorityEditForm($nodedata);
+        $priority = \Drupal::formBuilder()->getForm($priority_edit_form);
 
-         $status_edit_form = new MilestoneStatusEditForm($nodedata);
-         $status = \Drupal::formBuilder()->getForm($status_edit_form);
-         $table[$key] = [$title, $priority, $status];
+        $status_edit_form = new MilestoneStatusEditForm($nodedata);
+        $status = \Drupal::formBuilder()->getForm($status_edit_form);
+        $table[$key] = [$title, $priority, $status];
       }
 
     }
