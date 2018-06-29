cf16de90154de3c91b86afe6b7363fd4d9bee094
Andriy Iun <andriy.yun@gmail.com>
2018-06-29 14:04:03 +0200

    Added deployment file

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+DRUSH="drush --root=$(pwd)/docroot"
+DB_DUMP_FILE=/tmp/db_dump-$(date +%s).sql.gz
+$DRUSH sql-dump | gzip -9 >  $DB_DUMP_FILE
+echo "DB dumped to $DB_DUMP_FILE"
+composer install
+$DRUSH cim -y
+$DRUSH updb -y
+$DRUSH language-import da sites/default/po/da.po
