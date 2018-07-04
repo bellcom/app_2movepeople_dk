a397205b7f0d31aa4bf2dffc47d9c405f4b581fd
Andriy Iun <andriy.yun@gmail.com>
2018-07-04 21:31:50 +0200

    2M-254 Fixed evaluation reseting in ajax process update.

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/MilestoneEditForm.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/MilestoneEditForm.php
@@ -131,7 +131,6 @@ class MilestoneEditForm extends FormBase {
         $goal_node->set("title", $goal['title']);
         $goal_node->set("field_activity_title", $goal['activity_title']);
         $goal_node->set("field_due_date", $goal['due_date']);
-        $goal_node->set("field_evaluation", $goal['evaluation']);
         $goal_node->set("field_responsible_manager", $goal['responsible_manager']);
         $goal_node->save();
       }
