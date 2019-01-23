1d11bb9f4c3fd381d3a94494e7712b761c529475
Morten Nissen <mail@hjemmesidekongen.dk>
2019-01-23 14:49:39 +0100

    Hide navbar

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Controller/MovepeopleDashboardController.php
@@ -784,19 +784,21 @@ class MovepeopleDashboardController extends ControllerBase {
   public static function getControlButtons(array $links, array $attributes = []) {
     $build = [
       '#type' => 'container',
-      '#attributes' => array_merge_recursive($attributes, ['class' => ['controll-buttons']]),
+      '#attributes' => array_merge_recursive($attributes, ['class' => ['layout__sidebar__controls']]),
     ];
 
     foreach ($links as $key => $link) {
       $button = _bc_2movepeople_dashboard_button($key);
       $link['#title'] = $button['name'];
+
       if (!empty($button['title'])) {
         $link['#attributes']['title'] = $button['title'];
       }
+
       $build[$key] = array_merge_recursive($link, [
         '#type' => 'link',
         '#attributes' => [
-          'class' => ['btn', 'btn-default'],
+          'class' => ['btn', 'btn-default', 'btn-block'],
         ],
       ]);
     }
