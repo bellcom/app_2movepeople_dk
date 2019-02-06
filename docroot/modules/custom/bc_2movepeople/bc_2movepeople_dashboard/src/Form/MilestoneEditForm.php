75beeb985ce24b1121a3d6b29b5cfd2fa459a26e
inuitviking <runurth@gmail.com>
2019-02-06 16:29:30 +0100

    Adding missing 'this->'

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/MilestoneEditForm.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/MilestoneEditForm.php
@@ -90,7 +90,7 @@ class MilestoneEditForm extends FormBase {
         '#suffix' => '</div></div>',
         '#attributes' => [
           'class' => ['btn-default', 'glyphicon', 'glyphicon-refresh'],
-		  'title' => t('Update'),
+		  'title' => this->t('Update'),
         ],
         '#ajax' => [
           'callback' => '::ajaxSubmitForm',
