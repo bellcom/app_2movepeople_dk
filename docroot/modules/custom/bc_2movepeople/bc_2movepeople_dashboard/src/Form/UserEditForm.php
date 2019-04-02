97a7f7a5a9737859f1c61984caa06eaff375ed26
Morten Nissen <mail@hjemmesidekongen.dk>
2019-04-02 10:50:11 +0200

    2M-262 - add email address to user edit form

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/UserEditForm.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/UserEditForm.php
@@ -20,6 +20,9 @@ class UserEditForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, UserInterface $user = NULL) {
     $form_state->set('user', $user);
+    $form_state->setCached(FALSE);
+
+    $config = \Drupal::config('bc_2movepeople.settings');
 
     $form['#prefix'] = '<div id="bc_2movepeople-dashboard-user-edit-form">';
     $form['#suffix'] = '</div>';
@@ -29,22 +32,42 @@ class UserEditForm extends FormBase {
       '#weight' => -100,
     ];
 
+    // SSN.
     $form['social_security_number'] = [
       '#type' => 'textfield',
       '#placeholder' => $this->t('Social security number'),
-      '#default_value' => $user->get('field_social_security_number')->value
+      '#default_value' => $user->get('field_social_security_number')->value,
     ];
 
+    // Firstname.
     $form['firstname'] = [
       '#type' => 'textfield',
       '#placeholder' => $this->t('Firstname'),
-      '#default_value' => $user->get('field_user_firstname')->value
+      '#default_value' => $user->get('field_user_firstname')->value,
     ];
 
+    // Lastname.
     $form['surname'] = [
       '#type' => 'textfield',
       '#placeholder' => $this->t('Surname'),
-      '#default_value' => $user->get('field_user_surname')->value
+      '#default_value' => $user->get('field_user_surname')->value,
+    ];
+
+    // Username.
+    $form['username'] = [
+      '#type' => 'textfield',
+      '#placeholder' => $this->t('Username'),
+      '#required' => TRUE,
+      '#default_value' => $user->get('name')->value,
+    ];
+
+    // E-mail address.
+    $email_required = $config->get('email_required');
+    $form['email'] = [
+      '#type' => 'email',
+      '#placeholder' => $this->t('Email'),
+      '#required' => $email_required,
+      '#default_value' => $user->get('mail')->value,
     ];
 
     $form['actions'] = [
@@ -112,6 +135,7 @@ class UserEditForm extends FormBase {
       $user->set('field_social_security_number', $form_state->getValue('social_security_number'));
       $user->set('field_user_firstname', $form_state->getValue('firstname'));
       $user->set('field_user_surname', $form_state->getValue('surname'));
+      $user->set('mail', $form_state->getValue('email'));
 
       // Update user account.
       $user->save();
@@ -123,6 +147,7 @@ class UserEditForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $form_state->clearErrors();
+    $user = $form_state->get('user');
 
     // Check Firstname.
     $firstname = CommonFormUtils::cleanInput($form_state->getValue('firstname'));
@@ -134,6 +159,20 @@ class UserEditForm extends FormBase {
     $surname = CommonFormUtils::cleanInput($form_state->getValue('surname'));
     if (strlen($surname) < 2) {
       $form_state->setErrorByName('surname', $this->t('The Surname %surname is not valid.', ['%surname' => $surname]));
+    }
+
+    // Check Email.
+    $email = CommonFormUtils::cleanInput(trim($form_state->getValue('email')));
+    if ($form['email']['#required_but_empty']) {
+      $form_state->setErrorByName('email', $this->t('The Email address is required'));
+    }
+    elseif (!\Drupal::service('email.validator')->isValid($email) and !empty($email)) {
+      $form_state->setErrorByName('email', $this->t('The Email address %mail is not valid.', ['%mail' => $email]));
+    }
+    if ($user->get('mail')->value !== $email) {
+      if (!empty(user_load_by_mail($email))) {
+        $form_state->setErrorByName('email', $this->t('The Email address %mail already exists.', ['%mail' => $email]));
+      }
     }
   }
 
