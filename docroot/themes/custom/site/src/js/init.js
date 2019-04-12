521b73f38a8ddecdb7bc4d7fa4ae71e8a88c386c
Morten Nissen <mail@hjemmesidekongen.dk>
2019-04-12 10:24:55 +0200

    Remove unused slinky menu from project

--- docroot/themes/custom/site/src/js/init.js
+++ docroot/themes/custom/site/src/js/init.js
@@ -6,22 +6,12 @@
   pageLayout.init();
 
   // Sidr
-  $('.slinky-menu')
-    .find('ul, li, a')
-    .removeClass();
-
   $('.sidr-toggle--right').sidr({
     name: 'sidr-main',
     side: 'right',
     renaming: false,
     body: '.layout__wrapper',
     source: '.sidr-source-provider'
-  });
-
-  // Slinky
-  $('.sidr .slinky-menu').slinky({
-    title: true,
-    label: ''
   });
 
   // Enable / disable Bootstrap tooltips, based upon touch events
