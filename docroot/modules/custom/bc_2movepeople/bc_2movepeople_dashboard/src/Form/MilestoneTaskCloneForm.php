022135986e088fac0a611e6c6e205c3495347a4a
Andriy Iun <andriy.yun@gmail.com>
2020-09-29 18:23:07 +0200

    2M-335 Added ability to create new question on tack copy

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/MilestoneTaskCloneForm.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_dashboard/src/Form/MilestoneTaskCloneForm.php
@@ -38,19 +38,19 @@ class MilestoneTaskCloneForm extends FormBase {
     $progression_options = array();
     $entity_ids = MovepeopleDashboardController::getProgressionTargets($user->id());
     $progression_nodes = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($entity_ids);
-    
+
     foreach ($progression_nodes as $progrdata) {
       $progression_options[$progrdata->id()] = $progrdata->get('title')->value;
     }
-    
+
     $form['#prefix'] = '<div id="bc_2movepeople-dashboard-task-clone-form">';
     $form['#suffix'] = '</div>';
-    
+
     $form['system_messages'] = [
       '#markup' => '<div id="clone-task-form-system-messages"></div>',
       '#weight' => -100,
     ];
-    
+
     $form['progression_id'] = [
       '#type' => 'select',
       '#title' => $this->t('Category'),
@@ -64,20 +64,37 @@ class MilestoneTaskCloneForm extends FormBase {
         'wrapper' => 'parent_task_wrapper',
       ],
     ];
+    $task_ids = $this->getParentQuestionOptions($form_state);
+    if (!empty($task_ids)) {
+      $task_ids['other'] = $this->t('Anden ...');
+    }
     $form['parent_task_id'] = [
       '#type' => 'select',
       '#title' => $this->t('Parent question'),
-      '#options' => $this->getParentQuestionOptions($form_state),
+      '#options' =>  $task_ids,
       '#empty_option' => $this->t('-Select parent question-'),
       '#required' => FALSE,
       '#prefix' => '<div id="parent_task_wrapper">',
       '#suffix' => '</div>',
     ];
-    
+
+    $form['parent_task_other_title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Nyt spørgsmål'),
+      '#states' => [
+        'visible' => [
+          ':input[name=parent_task_id]' => ['value' => 'other'],
+        ],
+        'required' => [
+          ':input[name=parent_task_id]' => ['value' => 'other'],
+        ],
+      ]
+    ];
+
     $form['actions']['#type'] = 'actions';
     $form['actions']['submit'] = [
       '#type' => 'submit',
-      '#name' => 'submit',  
+      '#name' => 'submit',
       '#value' => $this->t('Save'),
       '#button_type' => 'primary',
       '#ajax' => [
@@ -95,18 +112,18 @@ class MilestoneTaskCloneForm extends FormBase {
   public function getFormId() {
     return 'bc_2movepeople-dashboard-task-clone-form';
   }
-  
+
   /**
    * AJAX callback handler that displays any errors or a success message.
    */
   public function ajaxSubmitForm(array $form, FormStateInterface $form_state) {
     $ajax_response = new AjaxResponse();
-   
+
     $message = [
       '#theme' => 'status_messages',
       '#message_list' => drupal_get_messages(),
     ];
-    
+
     if ($this->isSaved == SAVED_UPDATED) {
       $ajax_response->addCommand(new CloseModalDialogCommand());
       $ajax_response->addCommand(new HtmlCommand('#custom-form-system-messages', $message));
@@ -116,19 +133,19 @@ class MilestoneTaskCloneForm extends FormBase {
 
     return $ajax_response;
   }
-  
+
   /**
    * Ajax callback to change options for Parent Question.
    */
   public function changeParentQuestionOptionsAjax(array &$form, FormStateInterface $form_state) {
 	return $form['parent_task_id'];
   }
-  
+
   /**
    * Get options for Parent Question.
    */
   public function getParentQuestionOptions(FormStateInterface $form_state) {
-    
+
     $options = array();
     $progression_id = $form_state->getValue('progression_id');
 
@@ -141,7 +158,7 @@ class MilestoneTaskCloneForm extends FormBase {
   }
 
 
-  
+
   /**
    * {@inheritdoc}
    */
@@ -155,6 +172,7 @@ class MilestoneTaskCloneForm extends FormBase {
 
     $progression_id = $form_state->getValue('progression_id');
     $parent_task_id = $form_state->getValue('parent_task_id');
+    $parent_task_other_title = $form_state->getValue('parent_task_other_title');
 
     $new_node = Node::create(array(
       'type' => 'goal',
@@ -163,20 +181,42 @@ class MilestoneTaskCloneForm extends FormBase {
     ));
 
     if ($new_node->save() == SAVED_NEW) {
-      
+      if ($parent_task_id == 'other') {
+        $parent_task_node = Node::create(array(
+          'type' => 'goal',
+          'status' => 1,
+          'title' => $parent_task_other_title,
+        ));
+        $parent_task_node->save();
+
+        // Updating parent node with reference to newly created goal.
+        $new_progression_goal_ids = [];
+        $progression_node = Node::load($progression_id);
+        $old_progression_goal_ids = $progression_node->get('field_goal_ids')->getValue();
+        foreach($old_progression_goal_ids as $tid) {
+          $new_progression_goal_ids[] = $tid['target_id'];
+        }
+        $new_progression_goal_ids[] = $parent_task_node->id();
+
+        $progression_node->set('field_goal_ids', $new_progression_goal_ids);
+        $progression_node->save();
+
+        $parent_task_id = $parent_task_node->id();
+      }
+
       $node = $parent_task_id ? Node::load($parent_task_id) : Node::load($progression_id);
       $field_name = $parent_task_id ? 'field_subgoal' : 'field_goal_ids';
       $old_goal_ids = $node->get($field_name)->getValue();
-      
+
       $new_goal_ids = [];
       foreach($old_goal_ids as $tid) {
         $new_goal_ids[] = $tid['target_id'];
       }
       $new_goal_ids[] = $new_node->id();
-      
+
       $node->set($field_name, $new_goal_ids);
       $this->isSaved = $node->save();
-      
+
       if ($this->isSaved == SAVED_UPDATED) {
         drupal_set_message($this->t($this->updated_msg));
       } else {
@@ -186,7 +226,7 @@ class MilestoneTaskCloneForm extends FormBase {
       drupal_set_message($this->t($this->wrong_msg));
     }
   }
-  
+
   /**
    * {@inheritdoc}
    */
@@ -195,4 +235,5 @@ class MilestoneTaskCloneForm extends FormBase {
       $form_state->setErrorByName('progression_id', $this->t('Category field is required.'));
     }
   }
+
 }
