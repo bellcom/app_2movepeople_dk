d4b71821c23b744ccbbef932b6c70c36afc0ebe6
Andriy Iun <andriy.yun@gmail.com>
2020-11-09 08:28:35 +0100

    Fixed regression in rate fetching process

--- docroot/modules/custom/bc_2movepeople/bc_2movepeople_rate_progression/src/Controller/RateController.php
+++ docroot/modules/custom/bc_2movepeople/bc_2movepeople_rate_progression/src/Controller/RateController.php
@@ -45,7 +45,7 @@ class RateController extends ControllerBase {
     $dates = [];
     foreach ($goals as $key => $id) {
       $goal_id = $id;
-      $result = self::getTargetAveragePoints($progression_target_id, $goal_id, $date_from, $date_to);
+      $result = self::getTargetAveragePoints($progression_target_id, $goal_id, $date_from, $date_to, 5);
       if (empty($result)) {
         continue;
       }
@@ -110,7 +110,7 @@ class RateController extends ControllerBase {
    * @return array
    *   Average value of rate.
    */
-  public static function getTargetAveragePoints($target_id, $goal_id = NULL, $date_from = NULL, $date_to = NULL) {
+  public static function getTargetAveragePoints($target_id, $goal_id = NULL, $date_from = NULL, $date_to = NULL, $limit = NULL) {
     $query = \Drupal::database()->select('bc_2movepeople_rate_progression', 'rates');
     if ($goal_id) {
       $query->condition('goal_id', $goal_id, '=');
@@ -130,8 +130,8 @@ class RateController extends ControllerBase {
       $query->condition('created', $date_from, '>=');
     }
 
-    if (!isset($date_to) && !isset($date_from)) {
-      $query->range(0, 5);
+    if (!isset($date_to) && !isset($date_from) && $limit) {
+      $query->range(0, $limit);
     }
 
     $result = $query->execute()->fetchAll();
